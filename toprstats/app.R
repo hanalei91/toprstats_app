e25f5ae7476cd94f64c9d18fd6842e8c03ed0e4a
Hannah <92106289+hanalei91@users.noreply.github.com>
2022-12-08 16:59:24 -0600

    Add files via upload
    Updated app files

---
+++ toprstats/app.R
@@ -0,0 +1,42 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(DT)
+
+library(cfbfastR)
+library(cfbplotR)
+library(tidyverse)
+library(dplyr)
+
+player_usage <- read.csv("player_usage.csv",header = TRUE)
+
+ui <- fluidPage(
+  theme = shinytheme("cosmo"),
+  
+  navbarPage(
+    "TopRStats",
+    tabPanel("Player Usage")),
+  
+  titlePanel("College Football Player Usage: 2014-2022"),
+  
+  mainPanel(
+    DTOutput("table")))
+
+server = function(input,output) {
+  output$table <- renderDT(player_usage,
+                           filter = "top",
+                           options = list(
+                             pageLength = 25))}
+
+
+
+shinyApp(ui = ui, server = server)
